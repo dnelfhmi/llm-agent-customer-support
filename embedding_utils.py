811bc4428470eee80ac1b13eee6ecc6a74400d56
dnelfhmi <dnelfhmi@gmail.com>
2025-07-09 21:39:30 +1000

    Update deprecated openai embed api call

--- embedding_utils.py
+++ embedding_utils.py
@@ -1,11 +1,10 @@
-import openai
-import os
+from openai import OpenAI
 
-openai.api_key = os.getenv("OPENAI_API_KEY")
+client = OpenAI()
 
 def get_embedding(text):
-    response = openai.Embedding.create(
-        model="text-embedding-ada-002",
-        input=text
+    response = client.embeddings.create(
+        input=[text],
+        model="text-embedding-ada-002"
     )
-    return response['data'][0]['embedding']
+    return response.data[0].embedding
